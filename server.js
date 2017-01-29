9d69f3a7ac1838b8b04890c15bbacab1778c7c50
Slava Kovalyov <dev.vyacheslav@gmail.com>
2017-01-30 00:18:07 +0300

    рефакторинг: добавление котроллера и модели под методы Поиска/Удаления/Создания/Изменения записей

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// var db = require('.//db.js');
-var db = require('C:/openserver/domains/pureNodeApi/db.js');
-var ObjectID = require('mongodb').ObjectID;
-
+var db = require('C:/openserver/domains/pureNodeApi/db');
+var artistsConroller = require('C:/openserver/domains/pureNodeApi/controllers/artistsController');
 
 var app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,101 +14,18 @@ app.get('/', function (req, res) {
     res.send('Hello api');
 });
 
-
-var artists = [
-    {
-        id: 1,
-        name: 'Metallica'
-    },
-    {
-        id: 2,
-        name: 'Iron Maiden'
-    }
-];
-
 //получение списка
-app.get('/artists', function (req, res) {
-    db.get().collection('artists').find().toArray(
-        function (err, docs) {
-            if (err) {
-                console.log("err getArtists");
-                return res.sendStatus(500);
-            }
-        res.send(docs);
-        }
-    );
-});
+app.get('/artists',artistsConroller.all);
 
 // получение по айди
-app.get('/artists/:id', function (req, res) {
-    //т.к все id_ это объекты необходимо конвертировать
-    db.get().collection('artists').findOne(
-        {
-            _id: ObjectID(req.params.id)
-        },
-        function (err, doc) {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            res.send(doc)
-        }
-    );
-});
+app.get('/artists/:id', artistsConroller.findById);
 
-app.post('/artists', function (req, res) {
-    console.log(req.body);
-
-    var artist = {
-        // id: Date.now(),
-        name: req.body.name
-    };
-
-    // artists.push(artist);
-    db.get().collection('artists').insertOne(artist, function (err, result) {
-        if (err) {
-            console.log('err collection')
-            return res.sendStatus(500);
-        }
-        res.send(artist);
-    })
-});
+app.post('/artists', artistsConroller.create);
 
 //Находим объект по айди и обновляем его объектом с данными "name"
-app.put('/artists/:id', function (req, res) {
-    db.get().collection('artists').updateOne(
-        { _id: ObjectID(req.params.id) },
-        { name: req.body.name },
-        function (err, result) {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            res.sendStatus(200);
-        }
-    );
-});
+app.put('/artists/:id', artistsConroller.update);
 
-app.delete('/artists/:id', function (req, res) {
-    db.get().collection('artists').deleteOne(
-        {
-            _id: ObjectID(req.params.id)
-        },
-            function (err, result){
-                if(err)
-                {
-                    console.log(err);
-                    return res.sendStatus(500);
-                }
-                res.sendStatus(200);
-            }
-    );
-
-    // artists = artists.filter(function (artist) {
-    //     return artist.id !== Number(req.params.id);
-    // });
-    // res.sendStatus(200);
-});
+app.delete('/artists/:id', artistsConroller.delete);
 
 db.connect('mongodb://localhost:27017/api', function (err) {
     if (err)
